24d32b5e6a6b3c8d6f62d38d074ba8320f047ea7
Yisrael <yhsberman@gmail.com>
2024-10-09 00:35:24 +0300

    test: find error

--- app/frontend/src/keycloak.js
+++ app/frontend/src/keycloak.js
@@ -1,9 +1,10 @@
 import Keycloak from 'keycloak-js';
 
-const keycloakUrl = process.env.REACT_APP_KEYCLOAK_URL || 'http://localhost:8080';
+// const keycloakUrl = process.env.REACT_APP_KEYCLOAK_URL || 'http://localhost:8080';
 
 const keycloakConfig = {
-  url: keycloakUrl,
+  //url: keycloakUrl,
+  url: 'http://34.205.129.2:8443',
   realm: 'my-app-realm',
   clientId: 'my-app-client'
 };
