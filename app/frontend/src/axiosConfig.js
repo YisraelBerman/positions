f89a90cfcff73751da4f309ef3560c6625ef30ab
Yisrael <yhsberman@gmail.com>
2024-10-24 21:51:41 +0300

    feat: jenkinsfiles branch

--- app/frontend/src/axiosConfig.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const backendUrl = process.env.REACT_APP_BACKEND_URL || 'http://localhost:5000';
-const instance = axios.create({
-  baseURL: `${backendUrl}/api`,
-});
-
-export default instance;
